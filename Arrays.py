463c1f67afa525ea43a5a617a8258e208fa2eee5
DeepshikhanN <99959842+DeepshikhanN@users.noreply.github.com>
2022-05-17 14:57:55 +0530

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+arr=np.array([1,2,3])
+arr
+
+
+# In[3]:
+
+
+list_of_list=(([1,2,3]),[4,5,6])
+list_of_list
+
+
+# In[5]:
+
+
+np.arange(67)
+
+
+# In[6]:
+
+
+np.arange(36,56)
+
+
+# In[9]:
+
+
+np.zeros((2,6))
+
+
+# In[10]:
+
+
+np.linspace(0,20,10)
+
+
+# In[11]:
+
+
+np.eye(5)
+
+
+# In[12]:
+
+
+np.eye(3, dtype=int)
+
+
+# In[13]:
+
+
+np.random.rand(25)
+
+
+# In[3]:
+
+
+np.random.rand(0,25)
+
+
+# In[7]:
+
+
+np.random.rand(3,5)
+
+
+# In[12]:
+
+
+np.random.randint(3,5)
+
+
+# In[10]:
+
+
+np.random.randn(25)
+
+
+# In[16]:
+
+
+np.random.randint(0,10,15)
+
+
+# In[18]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
